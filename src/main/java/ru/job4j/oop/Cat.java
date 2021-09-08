42af057f9e7c1a703be80cf5dbcdf4f18a037a3a
Valdemar113 <daodando13@gmail.com>
2021-09-08 05:21:06 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -3,18 +3,15 @@ package ru.job4j.oop;
 public class Cat {
 
     private String food;
+    private String name;
 
     public void show() {
-
         System.out.println(this.name + this.food);
     }
 
     public void eat(String meat) {
-
         this.food = meat;
     }
-
-    private String name;
 
     public void giveNick(String nick) {
         this.name = nick;
