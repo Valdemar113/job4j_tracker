42df93d5ebb54906d583316407fa8df77459998d
Valdemar113 <daodando13@gmail.com>
2021-10-04 19:11:32 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -58,4 +58,15 @@ public class Tracker {
         return rsl;
 
     }
+
+    public boolean delete(int id) {
+     int index = indexOF(id);
+        boolean rsl = index != -1;
+        if (rsl) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+        }
+        return rsl;
+    }
 }
