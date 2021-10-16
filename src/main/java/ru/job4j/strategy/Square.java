0d4896d5f36458e6321b0612e386fa246cd772a4
Valdemar113 <daodando13@gmail.com>
2021-10-16 11:04:36 +0300

    7. Шаблон проектирования - Стратегия [#786]

--- src/main/java/ru/job4j/strategy/Square.java
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -4,12 +4,9 @@ public class Square implements Shape {
     @Override
     public String draw() {
         String ln = System.lineSeparator();
-        return   " ______ " + ln
-               + "|      |" + ln
-               + "|______|" + ln;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new Square().draw());
+        return " ______ " + ln
+                + "|      |" + ln
+                + "|______|" + ln;
     }
 }
+
