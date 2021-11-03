e3063bc20e5522a01e8873621035c2b51c3f7de0
Valdemar113 <daodando13@gmail.com>
2021-11-03 17:19:57 +0300

    1. Телефонный справочник на базе ArrayList [#41598 #223277]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -17,4 +17,14 @@ public class PhoneDictionaryTest {
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
 
+    @Test
+    public void whenFind0() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Egor");
+        assertThat(persons.size(), is(0));
+    }
+
 }
