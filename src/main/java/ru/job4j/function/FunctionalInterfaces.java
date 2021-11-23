50b777df492a767a855886d5fdd2cf0139772d26
Valdemar113 <daodando13@gmail.com>
2021-11-23 17:49:43 +0300

    1.2. Функциональный интерфейс [#247497]

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -9,17 +9,16 @@ import java.util.List;
 public class FunctionalInterfaces {
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<>();
-        BiConsumer<Integer, String> biCon = (s, s1) -> System.out.println(s + " " + s1);
-        map.put(1, "one");
-        map.put(2, "two");
-        map.put(3, "three");
-        map.put(4, "four");
-        map.put(5, "five");
-        map.put(6, "six");
-        map.put(7, "seven");
-        map.forEach(biCon);
+        BiConsumer<Integer, String> biCon = (s, s1) -> map.put(s, s1);
+        biCon.accept(1, "one");
+        biCon.accept(2, "two");
+        biCon.accept(3, "three");
+        biCon.accept(4, "four");
+        biCon.accept(5, "five");
+        biCon.accept(6, "six");
+        biCon.accept(7, "seven");
 
-        BiPredicate<Integer, String> biPred = (i, s) -> i % 2 == 0 || map.get(i).length() == 4;
+        BiPredicate<Integer, String> biPred = (i, s) -> i % 2 == 0 || s.length() == 4;
         for (Integer i : map.keySet()) {
             if (biPred.test(i, map.get(i))) {
                 System.out.println("key: " + i + " value: " + map.get(i));
@@ -32,7 +31,7 @@ public class FunctionalInterfaces {
         List<String> strings = sup.get();
         Function<String, String> func = s -> s.toUpperCase();
         for (String s : strings) {
-            System.out.println(func.apply(s));
+            con.accept(func.apply(s));
         }
     }
 
